f61c2930048c97de139b34a7da5309025af268f6
Patryk Raźny <kontakt@patryk-razny.pl>
2023-01-05 16:14:47 +0100

    Install React and refine.

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  Layout,
+  ErrorComponent,
+  ReadyPage,
+  LightTheme,
+  CssBaseline,
+  ThemeProvider,
+  GlobalStyles,
+  RefineSnackbarProvider,
+  notificationProvider,
+} from "@pankod/refine-mui";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+const App: React.FC = () => {
+  return (
+      <ThemeProvider theme={LightTheme}>
+        <CssBaseline />
+        <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+        <RefineSnackbarProvider>
+          <Refine
+              routerProvider={routerProvider}
+              dataProvider={dataProvider(
+                  "https://api.fake-rest.refine.dev",
+              )}
+              notificationProvider={notificationProvider}
+              Layout={Layout}
+              ReadyPage={ReadyPage}
+              catchAll={<ErrorComponent />}
+          />
+        </RefineSnackbarProvider>
+      </ThemeProvider>
+  );
+};
+
+export default App;
