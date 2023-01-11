0cc0fdc567d427c1f6ec68bd14c54bc2fe782829
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2023-01-11 19:29:47 +0100

    add activity interface

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,3 +1,34 @@
+import internal from "stream";
+
 export interface IFaculty {
     name: string;
+}
+
+export interface ICourse {
+    id: number;
+    name: string;
+    ects: number;
+    cnps: number;
+    zzu: number;
+    bu: number;
+    hours_count: number;
+    code: string;
+    type: Type;
+    completing_form: string
+    course_form: Form
+}
+
+enum Type {
+    laboratory,
+    practice,
+    lecture,
+    project,
+    seminar,
+    lang_course,
+    thesis
+}
+
+enum Form {
+    stationary,
+    remote
 }
