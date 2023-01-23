e318f51e1628b3a9f3fcffb953252cb9113e805e
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2023-01-23 15:57:13 +0100

    correct opinions show

--- src/pages/opinions/show.tsx
+++ src/pages/opinions/show.tsx
@@ -14,9 +14,9 @@ import {
   DataGrid,
   useDataGrid,
   GridColumns,
+  TextareaAutosize,
 } from "@pankod/refine-mui";
 import { IPlans, IOpinion, IUser } from "../../interfaces";
-
 
 export const OpinionShow: React.FC = () => {
   const { queryResult } = useShow<IOpinion>();
@@ -37,6 +37,7 @@ export const OpinionShow: React.FC = () => {
   const plan_details = plan_data.data?.data;
   console.log(user_details);
   console.log(plan_details);
+
   const show = [];
 
   for (var record_elem in record) {
@@ -44,7 +45,8 @@ export const OpinionShow: React.FC = () => {
       record_elem == "id" ||
       record_elem == "opinions" ||
       record_elem == "field_id" ||
-      record_elem == "semesters"
+      record_elem == "semesters" ||
+      record_elem == "description"
     )
       continue;
 
@@ -53,7 +55,18 @@ export const OpinionShow: React.FC = () => {
     if (record_elem != "")
       record_elem = (record_elem as string).replaceAll("_", " ");
 
-    if (record_elem == "lang") record_elem = "Language";
+    if (record_elem == "is approved")
+      value_record = value_record ? "true" : "false"
+
+    if (record_elem == "user id") {
+      record_elem = record_elem + " " + user_details?.login
+      value_record = value_record + " " + user_details?.email
+    }
+
+    if (record_elem == "plan id") {
+      record_elem = "plan" + " " + plan_details?.id + " year " + plan_details?.year
+      value_record = plan_details?.form + " " + plan_details?.lang 
+    }
 
     show.push(
       <Grid item xs={6} key={record_elem}>
@@ -79,6 +92,28 @@ export const OpinionShow: React.FC = () => {
       </Grid>
     );
   }
+
+  show.push(
+    <Grid item xs={1} key="Description">
+        <Card
+          sx={{ minWidth: 850 }}
+          style={{ flex: 1, backgroundColor: "#353434" }}
+        >
+          <CardContent>
+          <Stack
+              gap={1}
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              spacing={2}
+            >
+              <TextField variant="h6" value="Description:" />
+              <MarkdownField value={record?.description} />
+            </Stack>
+          </CardContent>
+        </Card>
+      </Grid>
+  );
 
   return (
     <>
