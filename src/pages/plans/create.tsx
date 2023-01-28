f2154920c15d12e6735f414e0ed51a972ae2416d
prazny <kontakt@patryk-razny.pl>
2023-01-28 14:25:55 +0100

    Add semester inputs in create plan

--- src/pages/plans/create.tsx
+++ src/pages/plans/create.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
     useDataGrid,
     DataGrid,
@@ -7,7 +7,7 @@ import {
     Create,
     TextField,
     Box,
-    Autocomplete, useAutocomplete, Select, MenuItem, InputLabel, FormControl, Grid
+    Autocomplete, useAutocomplete, Select, MenuItem, InputLabel, FormControl, Grid, Input, Typography
 } from "@pankod/refine-mui";
 import {useForm, Controller} from "@pankod/refine-react-hook-form";
 import {IPlans} from "../../interfaces";
@@ -20,12 +20,29 @@ export const PlanCreate: React.FC = () => {
         refineCore: {formLoading},
         register,
         control,
-        formState: {errors},
+        formState: {errors,},
     } = useForm();
+    const [semestersDetails, setSemestersDetails] = useState<{id: number, ects: number}[]>([])
+
 
     const {autocompleteProps: fieldAutocompleteProps} = useAutocomplete({
         resource: "fields",
     });
+
+    function setSemesterCount(newLen: number) {
+        let len = semestersDetails.length
+        let newSemestersDetails = semestersDetails;
+        if (newLen > len) {
+            newSemestersDetails = semestersDetails
+            while (len < newLen) {
+                newSemestersDetails = [...newSemestersDetails, {"id": len, "ects": 0}]
+                len = len + 1
+            }
+            setSemestersDetails(newSemestersDetails)
+        } else if (newLen < len) {
+            setSemestersDetails(semestersDetails.slice(0, newLen))
+        }
+    }
 
 
     return (
@@ -36,7 +53,6 @@ export const PlanCreate: React.FC = () => {
                     sx={{display: "flex", flexDirection: "column"}}
                     autoComplete="off"
                 >
-
                     <Controller control={control}
                                 name="field_id"
                                 rules={{required: "This field is required"}}
@@ -95,6 +111,7 @@ export const PlanCreate: React.FC = () => {
                                 {...register("number_of_semesters", {
                                     required: "This field is required",
                                 })}
+                                onChange={value => setSemesterCount(parseInt(value.target.value))}
                                 error={!!(errors as any)?.title}
                                 helperText={(errors as any)?.title?.message}
                                 margin="normal"
@@ -124,8 +141,8 @@ export const PlanCreate: React.FC = () => {
                             <Controller
                                 control={control}
                                 name="form"
-                                rules={{ required: "This field is required" }}
-                                render={({ field }) => (
+                                rules={{required: "This field is required"}}
+                                render={({field}) => (
                                     <Autocomplete
                                         options={["fulltime", "parttime"]}
                                         getOptionLabel={(item) => {
@@ -151,6 +168,42 @@ export const PlanCreate: React.FC = () => {
                                     />
                                 )}
                             />
+
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography gutterBottom variant="h5">Semestry</Typography>
+                            {semestersDetails.length == 0 &&
+                                <Typography>Wpisz liczbę semestrów</Typography>
+                            }
+
+                            {semestersDetails.map(item => {
+                                return (
+                                    <Grid key={item.id} container spacing={2}>
+                                        <Grid item xs={2}>
+                                            <TextField
+                                                margin="normal"
+                                                fullWidth
+                                                InputLabelProps={{shrink: true}}
+                                                type="text"
+                                                value={item.id +1}
+                                                label="Semestr"
+                                                name="ects"
+                                                disabled
+                                            />
+                                        </Grid>
+                                        <Grid item xs={3}>
+                                            <TextField
+                                                margin="normal"
+                                                fullWidth
+                                                InputLabelProps={{shrink: true}}
+                                                type="text"
+                                                label="Deficyt ECTS"
+                                                name="ects"
+                                            />
+                                        </Grid>
+                                    </Grid>
+                                )
+                            })}
 
                         </Grid>
                     </Grid>
